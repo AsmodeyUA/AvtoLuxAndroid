65b1f42e2208addfa6bfca947e315d617929b25a
AsmodeyZ <AnatoliySerheyev@gmail.com>
2015-11-26 03:43:33 +0200

    new commit1

--- src/ua/uz/avtolux/MainActivity.java
+++ src/ua/uz/avtolux/MainActivity.java
@@ -2,6 +2,7 @@ package ua.uz.avtolux;
 
 import android.support.v7.app.ActionBarActivity;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
@@ -29,10 +30,12 @@ public class MainActivity extends ActionBarActivity {
         button.setId(1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(final View v) {
-        		setContentView(R.layout.activity_login);
+        		
                 Toast.makeText(getApplicationContext(),
                         "Buttonclicked: ",
                         Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
             	}
             });
         final TableLayout llayer = new TableLayout(this);
